b50f9a22ca3833f49e52e43853c00c9009edf8db
Igor Costa <igorsantoscosta@gmail.com>
2024-03-22 16:53:09 -0300

    feat: create vote use case

---
+++ src/application/use-cases/rooms/vote-room.service.ts
@@ -0,0 +1,61 @@
+import { RoomsRepository } from '@/application/repositories/rooms.repository';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Member } from '@prisma/client';
+import {
+  ROOM_NOT_FOUND,
+  USER_WITHOUT_PERMISSION,
+} from '@/application/errors/errors.constants';
+import { MembersRepository } from '@/application/repositories/members.repository';
+
+export interface VoteRoomUseCaseResponse {
+  member: Member;
+}
+
+interface VoteRoomServiceExecuteProps {
+  userId: string;
+  roomId: string;
+  vote: string;
+}
+
+@Injectable()
+export class VoteRoomService {
+  constructor(
+    private roomsRepository: RoomsRepository,
+    private membersRepository: MembersRepository,
+  ) {}
+
+  async execute(
+    data: VoteRoomServiceExecuteProps,
+  ): Promise<VoteRoomUseCaseResponse> {
+    const roomsExists = await this.roomsRepository.findById(data.roomId);
+
+    if (!roomsExists) throw new BadRequestException(ROOM_NOT_FOUND);
+
+    const userActionIsOwnerTheRoom = roomsExists.owner_id !== data.userId;
+
+    if (!userActionIsOwnerTheRoom) {
+      const userActionIsInsideTheRoom =
+        await this.membersRepository.findByMemberAndRoomId({
+          memberId: data.userId,
+          roomId: data.roomId,
+        });
+
+      if (!userActionIsInsideTheRoom)
+        throw new UnauthorizedException(USER_WITHOUT_PERMISSION);
+    }
+
+    const memberVoted = await this.membersRepository.update(
+      {
+        memberId: data.userId,
+        roomId: data.roomId,
+      },
+      { vote: data.vote },
+    );
+
+    return { member: memberVoted };
+  }
+}
