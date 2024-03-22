c215071dcc98cd7fd748ab89b4af5dce47577ed3
Igor Costa <igorsantoscosta@gmail.com>
2024-03-21 23:53:52 -0300

    refactor: create validations constants file

---
+++ src/application/errors/validations.constants.ts
@@ -0,0 +1,19 @@
+export const NAME_ERROR_MESSAGE = 'O nome deve ser uma string.';
+
+export const USER_NAME_ERROR_MESSAGE = 'O nome de usuário deve ser uma string.';
+
+export const LATITUDE_NUMBER_ERROR_MESSAGE = 'A latitude deve ser um número.';
+
+export const LONGITUDE_NUMBER_ERROR_MESSAGE = 'A longitude deve ser um número.';
+
+export const LATITUDE_MIN_ERROR_MESSAGE =
+  'A latitude deve ser maior ou igual a -90 graus.';
+
+export const LATITUDE_MAX_ERROR_MESSAGE =
+  'A latitude deve ser menor ou igual a 90 graus.';
+
+export const LONGITUDE_MIN_ERROR_MESSAGE =
+  'A longitude deve ser maior ou igual a -180 graus.';
+
+export const LONGITUDE_MAX_ERROR_MESSAGE =
+  'A longitude deve ser menor ou igual a 180 graus.';
