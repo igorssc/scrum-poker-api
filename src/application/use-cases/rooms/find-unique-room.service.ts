48c6b442917cbb814f295d4d8d0519b7ee305fc3
Igor Costa <igorsantoscosta@gmail.com>
2025-10-25 13:06:03 -0300

    fix: improve error handling in FindUniqueRoomService by adding ROOM_NOT_FOUND exception

--- src/application/use-cases/rooms/find-unique-room.service.ts
+++ src/application/use-cases/rooms/find-unique-room.service.ts
@@ -1,4 +1,4 @@
-import { INVALID_PARAMS } from '@/application/errors/errors.constants';
+import { INVALID_PARAMS, ROOM_NOT_FOUND } from '@/application/errors/errors.constants';
 import { RoomsRepository } from '@/application/repositories/rooms.repository';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Room } from '@prisma/client';
@@ -13,12 +13,16 @@ export class FindUniqueRoomService {
   constructor(private roomsRepository: RoomsRepository) {}
 
   async execute(query: string): Promise<FindUniqueRoomUseCaseResponse> {
-    if (isUUID(query)) {
-      const room = await this.roomsRepository.findById(query, true);
-
-      return { room };
+    if (!isUUID(query)) {
+      throw new BadRequestException(INVALID_PARAMS);
     }
 
-    throw new BadRequestException(INVALID_PARAMS);
+    const room = await this.roomsRepository.findById(query, true);
+
+    if(!room) {
+      throw new BadRequestException(ROOM_NOT_FOUND);
+    }
+
+    return { room };
   }
 }
