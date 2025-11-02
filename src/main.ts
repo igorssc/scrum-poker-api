64e0ef898d87d525d2e708638478a4730bc0ac6a
Igor Costa <igorsantoscosta@gmail.com>
2025-11-02 02:17:05 -0300

    fix: remove explicit host binding in server listen method

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,8 @@ async function bootstrap() {
   app.enableCors(corsOptions);
 
   const PORT = process.env.PORT || 3000;
-  
-  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 }
 bootstrap();
