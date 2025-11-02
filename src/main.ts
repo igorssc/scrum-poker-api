e59fb8d14fad55a636add74c531c9febd5f62a09
Igor Costa <igorsantoscosta@gmail.com>
2025-11-02 02:12:35 -0300

    fix: update server listen logic to use environment variable for port configuration

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,9 @@ async function bootstrap() {
 
   app.enableCors(corsOptions);
 
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
 }
 bootstrap();
