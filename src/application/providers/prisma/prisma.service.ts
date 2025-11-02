c2f3d408489f159fd0bd0d6f8dfc6107d2069022
Igor Costa <igorsantoscosta@gmail.com>
2025-11-02 11:44:33 -0300

    feat: implement OnModuleDestroy lifecycle hook in PrismaService for proper resource cleanup

--- src/application/providers/prisma/prisma.service.ts
+++ src/application/providers/prisma/prisma.service.ts
@@ -1,9 +1,26 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private static instance: PrismaService;
+
+  constructor() {
+    if (PrismaService.instance) {
+      return PrismaService.instance;
+    }
+    super();
+    PrismaService.instance = this;
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
